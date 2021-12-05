0a11fa36ea90fc4595409877ba1635141429e0da
bot <bot6324@outlook.sk>
2021-12-05 12:06:26 +0800

    Update mtproxy_go.sh

--- mtproxy_go.sh
+++ mtproxy_go.sh
@@ -106,10 +106,13 @@ Download(){
 	else
 		bit="arm"
 	fi
-	wget --no-check-certificate -N "https://github.com/9seconds/mtg/releases/download/${new_ver}/mtg-linux-${bit}"
-	[[ ! -e "mtg-linux-${bit}" ]] && echo -e "${Error} MTProxy 下载失败 !" && rm -rf "${file}" && exit 1
-	mv "mtg-linux-${bit}" "mtg"
-	[[ ! -e "mtg" ]] && echo -e "${Error} MTProxy 重命名失败 !" && rm -rf "${file}" && exit 1
+	new_ver=$(wget -qO- https://api.github.com/repos/9seconds/mtg/releases| grep "tag_name"| head -n 1| awk -F ":" '{print $2}'| sed 's/\"//g;s/,//g;s/ //g')
+	[[ -z ${new_ver} ]] && echo -e "${Error} MTProxy 最新版本获取失败！" && exit 1
+	echo -e "${Info} 检测到 MTProxy 最新版本为 [ ${new_ver} ]"
+	wget --no-check-certificate -N "https://github.com/9seconds/mtg/releases/download/${new_ver}/mtg-${new_ver:1}-linux-${bit}.tar.gz"
+	[[ ! -e "mtg-${new_ver:1}-linux-${bit}.tar.gz" ]] && echo -e "${Error} MTProxy 下载失败 !" && rm -rf "${file}" && exit 1
+	tar -xzf mtg-${new_ver:1}-linux-${bit}.tar.gz && cd mtg-${new_ver:1}-linux-${bit} &&  mv "mtg" .. && rm -rf mtg-${new_ver:1}-linux-${bit}
+	cd .. && [[ ! -e "mtg" ]] && echo -e "${Error} MTProxy 移动失败 !" && rm -rf "${file}" && exit 1
 	chmod +x mtg
 	echo "${new_ver}" > ${Now_ver_File}
 }
